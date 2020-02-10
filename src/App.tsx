d5a3aa7cd95a7b780c021a3903ff3cd84c5b4c8c
Paperplastic <xxl-0011@hotmail.com>
2020-02-10 18:29:46 +0800

    react router to use hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useReducer } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { openDB } from "./cart/CartLogic";
 import { ICart, IFilter } from "./data/Interfaces";
 import { filters, reducer } from "./store/filters/FilterReducer";
